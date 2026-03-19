2c51ccf37e3cf31974714292bac762726d9387ee
mMockyyy <mmlconcepcion@tip.edu.ph>
2026-03-19 16:22:36 +0800

    Configure Render Python runtime and build command

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 
 set -o errexit # exit on error
 
-pip install -r requirements.txt
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
